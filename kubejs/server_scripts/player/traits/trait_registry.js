d87892e18676b31242b7afdf1de981c05fd4bc23
Sterling <benevolent0377@gmail.com>
2026-06-22 17:23:22 -0500

    renamed TRAIT class to Trait and fixed some other issues with the class

--- kubejs/server_scripts/player/traits/trait_registry.js
+++ kubejs/server_scripts/player/traits/trait_registry.js
@@ -1,10 +1,10 @@
 // this file will hold all of the basic trait data
 import { CONSTANTS } from "../../core/constants";
 
-class TRAIT {
+class Trait {
     constructor(id, display, DEFAULT_BASE, description) {
         this.id = id,
-        this.display = display
+        this.display = display,
         this.DEFAULT_BASE = DEFAULT_BASE,
         this.description = description
     }
@@ -19,11 +19,11 @@ class TRAIT {
 }
 
 export const TRAITS = {
-    STRENGTH: new TRAIT(CONSTANTS.TRAIT_ID.STR, "Strength", 10, ""),
-    CONSTITUTION: new TRAIT(CONSTANTS.TRAIT_ID.CONS, "Constitution", 10, ""),
-    ENDURANCE: new TRAIT(CONSTANTS.TRAIT_ID.END, "Endurance", 10, ""),
-    AGILITY: new TRAIT(CONSTANTS.TRAIT_ID.AGL, "Agility", 10, ""),
-    DEXTERITY: new TRAIT(CONSTANTS.TRAIT_ID.DEX, "Dexterity", 10, ""),
-    INTELLIGENCE: new TRAIT(CONSTANTS.TRAIT_ID.INT, "Intelligence", 10, ""),
-    WISDOM: new TRAIT(CONSTANTS.TRAIT_ID.WIS, "Wisdom", 10, ""),
+    STRENGTH: new Trait(CONSTANTS.TRAIT_ID.STR, "Strength", 10, ""),
+    CONSTITUTION: new Trait(CONSTANTS.TRAIT_ID.CONS, "Constitution", 10, ""),
+    ENDURANCE: new Trait(CONSTANTS.TRAIT_ID.END, "Endurance", 10, ""),
+    AGILITY: new Trait(CONSTANTS.TRAIT_ID.AGL, "Agility", 10, ""),
+    DEXTERITY: new Trait(CONSTANTS.TRAIT_ID.DEX, "Dexterity", 10, ""),
+    INTELLIGENCE: new Trait(CONSTANTS.TRAIT_ID.INT, "Intelligence", 10, ""),
+    WISDOM: new Trait(CONSTANTS.TRAIT_ID.WIS, "Wisdom", 10, ""),
 }

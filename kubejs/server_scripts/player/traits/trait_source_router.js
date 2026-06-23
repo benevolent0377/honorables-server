1573ac273fdfe90f6d541fbb5e622c2c4f2c334a
Sterling <benevolent0377@gmail.com>
2026-06-23 00:34:55 -0500

    cleaned up trait_source_router.js

--- kubejs/server_scripts/player/traits/trait_source_router.js
+++ kubejs/server_scripts/player/traits/trait_source_router.js
@@ -4,31 +4,19 @@ export function getValueFromSource(player, source, factor) {
 
     const sourceTypes = CONSTANTS.TRAIT_FACTOR_TYPES.LIST;
 
-    var isFound = false;
-
-    for (const sourceType of sourceTypes) {
-        if (source == sourceType) {
-            isFound = true;
-        }
-    }
-
-    if (!isFound) {
-        return undefined;
-    }
-
     switch (source) {
 
         case "vanilla_stat":
             return readVanillaStat(player, factor);
-            break;
         
         case "player_history":
             return readPlayerHistory(player, factor);
-            break;
 
         case "stage":
             return hasStage(player, factor);
-            break;
+
+        default:
+            return undefined;
     }
 
 }

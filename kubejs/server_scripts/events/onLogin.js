986288f7a65a3306a285568d8922daedf4a295b2
Sterling <benevolent0377@gmail.com>
2026-06-28 19:47:55 -0500

    beginning developemnt on the player login listener

--- kubejs/server_scripts/events/onLogin.js
+++ kubejs/server_scripts/events/onLogin.js
@@ -1,5 +1,12 @@
+import { playerHasRoot } from "../core/presistence";
+
 PlayerEvents.onLogin(event => {
 
     // login functions go here
 
+    //validate player data:
+    playerHasRoot(event.player, true);
+
 });
+
+// the above event hook is meant to call functions in this file. it should not have its own operations hardcoded.

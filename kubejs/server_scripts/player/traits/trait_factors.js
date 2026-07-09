ab8e47c593531f2100545ddb3dfe46f1c64ce5c7
Sterling <benevolent0377@gmail.com>
2026-07-09 10:58:48 -0500

    added static weight values to the trait factors so weighting can be easily altered

--- kubejs/server_scripts/player/traits/trait_factors.js
+++ kubejs/server_scripts/player/traits/trait_factors.js
@@ -5,14 +5,76 @@ var ROOT = global.HonorablesRoot || global.Honorables;
 
 ROOT.TraitFactors = ROOT.TraitFactors || {};
 
+ROOT.Traits.Factors.StatWeightRegistry = {
+
+    EX_LOW: .0005,
+    LOW: .001,
+    EX_SMALL: .005,
+    SMALL: .01,
+    MODERATE: .025,
+    LARGE: .1,
+    EX_LARGE: .2,
+    SIGNIFICANT: .5,
+    EX_SIGNIFICANT: .10,
+
+    DOUBLE: function(weight) {
+        return weight*2;
+    },
+
+    GROW_SHRINK: function(value, factor) {
+        return value * 10^(factor);
+    },
+
+
+    /**
+     * Vary weighting of a factor based on the amount of a stat a player has upon calculation.
+     * 
+     * @param statQty {integer} The exact number of any given stat the player has at calculation.
+     * @param direction {string} Either positive or negative. Controls the direction of the exponent.
+     * @param factor {float} A number from 1 to N. The multiplicative increase or decrease in slope of the exponent.
+     * @param padX {float} A number from 0 to N. Skews the function graphically on the X-axis.
+     * @param padY {float} A number from 0 to N. Skews the function grapically on the Y-axis.
+     * 
+     * @return newWeight {number} The new weight value for the factor.
+     */
+    EXPONENTIALIZE: function(statQty, direction, factor, padX, padY){
+
+        if (factor < 1) {
+            factor = 1;
+        }
+
+        switch (direction.toLowerCase()) {
+            case "positive" || "pos":
+                var directionVector = 1;
+            break;
+
+            case "negative" || "neg":
+                var directionVector = -1;
+            break
+
+            default:
+                return itemWeight;
+
+        }
+
+        var newWeight = Number(statQty^(((1/2)*factor + direction)/(factor + padX)) + padY);
+        var newWeight = Number(newWeight, -3);
+
+        return newWeight;
+    }
+
+}
+
 ROOT.TraitFactors.factorWeights = {
+
+    REGISTRY: ROOT.Traits.Factors.StatWeightRegistry,
     // WEIGHT applies to the whole source category; SUBFACTOR_WEIGHTS tune individual factors.
     VANILLA_STATS: {
         WEIGHT: .35,
         SUBFACTOR_WEIGHTS: {
             //various subfactors and their weights go here
-            "minecraft:damage_dealt": .02,
-            "minecraft:killed": .05
+            "minecraft:damage_dealt": REGISTRY.EX_LOW,
+            "minecraft:killed": REGISTRY.LOW
         }
     },
 

c77cc50194ecb253d33b417a40be624d9fbe9af6
Sterling <benevolent0377@gmail.com>
2026-07-15 13:40:57 -0500

    renamed trait.js in the notifications directory to be called notification_registry.js and started building notification constructor

---
+++ kubejs/server_scripts/notifications/notification_registry.js
@@ -0,0 +1,16 @@
+global.Honorables = global.Honorables || {};
+var ROOT = global.HonorablesRoot || global.Honorables;
+
+ROOT.Notifications = ROOT.Notifications || {};
+
+ROOT.Notifications.Notification = function(){
+
+    // put all the vars the notification will need here
+
+}
+
+ROOT.Notifications.Notification.prototype.Run = function(){
+
+    // the command run goes here
+
+}
